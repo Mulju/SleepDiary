d9ddb757491bb6f5e525463df2fbaebc7104e047
pyy <Pyry.Perala@metropolia.fi>
2021-10-04 14:37:48 +0300

    layout kerroyostasi paranneltu

--- app/src/main/java/com/example/unipaivakirja_haltimo_backman_perala/KerroYostasi.java
+++ app/src/main/java/com/example/unipaivakirja_haltimo_backman_perala/KerroYostasi.java
@@ -3,12 +3,19 @@ package com.example.unipaivakirja_haltimo_backman_perala;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class KerroYostasi extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kerro_yostasi);
+
+
     }
+
+
+
 }
