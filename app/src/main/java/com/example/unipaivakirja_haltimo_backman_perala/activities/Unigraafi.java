5f1f67b914007b944a05bd0519ebbe8e9560957a
Mulju <samuli.matias@hotmail.com>
2021-10-11 14:23:18 +0300

    Unigraafin javadoc kirjoitettu

--- app/src/main/java/com/example/unipaivakirja_haltimo_backman_perala/activities/Unigraafi.java
+++ app/src/main/java/com/example/unipaivakirja_haltimo_backman_perala/activities/Unigraafi.java
@@ -27,7 +27,7 @@ import com.jjoe64.graphview.series.LineGraphSeries;
  */
 
 public class Unigraafi extends AppCompatActivity {
-    // Määritetään graafi ja graafin pisteet
+    // Alustetaan graafi ja graafin pisteet
     GraphView graphView;
     LineGraphSeries<DataPoint> series;
 
@@ -36,19 +36,27 @@ public class Unigraafi extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_unigraafi);
 
-        // Etsitään graafi elementti
+        // Etsitään graafin view-elementti
         graphView = (GraphView) findViewById(R.id.graph);
-        // Määritetään mitkä pisteet graafi piirtää
-        series= new LineGraphSeries<DataPoint>(data());
+        // Määritetään mitkä pisteet graafi piirtää data()-metodin avulla
+        series = new LineGraphSeries<DataPoint>(data());
         // Lisätään pisteet graafiin
         graphView.addSeries(series);
     }
-    /*
-    *
+
+    /**
+     * Tässä metodissa asetetaan DataPoint-olioon graafin piirtoa varten halutut pisteet.
+     * Metodissa myös toteutus, jolla graafiin piirtyy maksimissaan 20 data pistettä.
+     *
+     * @return palauttaa DataPoint-olion, joka sisältää halutut datapisteet.
      */
     public DataPoint[] data(){
+        /** X-akselin pituutta hallinnoiva muuttuja. */
         int x_axis;
+        /** Muuttuja, jolla liikutetaan listan indeksiä, mikäli listassa yli 20 alkiota. */
         int offSet;
+
+        // Tarkistetaan onko YoData-singletonin listassa yli 20 alkiota.
         if (YoData.getInstance().getYot().size() > 20) {
             x_axis = 20;
             offSet = YoData.getInstance().getYot().size() - 20;
@@ -57,7 +65,10 @@ public class Unigraafi extends AppCompatActivity {
             offSet = 0;
         }
 
+        // Luodaan DataPoint-olio, johon talletetaan x_axis muuttujan määräämän verran datapisteitä.
         DataPoint[] dataPisteet = new DataPoint[x_axis];
+        // For-lause, jolla käydään YoData-luokan listasta nukuttuja tunteja,
+        // ja lisätään ne datapisteiksi.
         for (int i = 0 + offSet; i < x_axis; i++){
             DataPoint yksittäinenPiste = new DataPoint(i, YoData.getInstance().getYot().get(i).getNukututTunnit());
             dataPisteet[i] = yksittäinenPiste;
@@ -65,4 +76,3 @@ public class Unigraafi extends AppCompatActivity {
         return dataPisteet;
     }
 }
-
